08b3fdfc2f33cd03aa4a73417b24713da58d6b3c
diname <30847683+diname@users.noreply.github.com>
2017-11-08 20:58:57 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,3 +13,10 @@ class Curso(models.Model):
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
 
+    
+class Candidato (models.Model):
+    nome = models.CharField(max_length=200)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    endereço = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=11)
