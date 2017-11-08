d68a58174232ece729e46b2c6a25e79efc999f31
diname <30847683+diname@users.noreply.github.com>
2017-11-08 20:36:09 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -17,4 +17,5 @@ class Candidato (models.Model):
     nome = models.CharField(max_length=200)
     rg = models.CharField(max_length=9)
     cpf = models.CharField(max_length=11)
+    endereço = models.CharField(max_length=200)
     telefone = models.CharField(max_length=11)
